25c4d63569bc8dc0ed3fa441eda4c5f160532218
m_davidV <vinaarizadavid@gmail.com>
2024-01-17 16:13:25 -0500

    integracion variables y botones como componente

--- src/contexts/App.jsx
+++ src/contexts/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "../assets/react.svg";
+import viteLogo from "/vite.svg";
+import "../scss/_main.scss";
+
+import "../App.css";
+import { Button, Col, Container, Row } from "reactstrap";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,20 +19,88 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="titulo">
+        Find Real Estate <br />
+        That Suits You.
+      </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="btn btn-primary"
+        >
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-asd">
         Click on the Vite and React logos to learn more
       </p>
+      <Container>
+        <Row>
+          <Col>
+            <a href="#" className="primary-button-xl m-3">
+              Button
+            </a>
+
+            <a href="#" className="primary-button-l m-3">
+              Button
+            </a>
+
+            <a href="#" className="primary-button-sm m-3">
+              Button
+            </a>
+
+            <a href="#" className="primary-button-xsm m-3">
+              Button
+            </a>
+          </Col>
+        </Row>
+
+        <Row className="m-5">
+          <Col>
+            <a href="#" className="secondary-button-xl m-3">
+              Button
+            </a>
+
+            <a href="#" className="secondary-button-l m-3">
+              Button
+            </a>
+
+            <a href="#" className="secondary-button-sm m-3">
+              Button
+            </a>
+
+            <a href="#" className="secondary-button-xsm m-3">
+              Button
+            </a>
+          </Col>
+        </Row>
+
+        
+        <Row className="m-5">
+          <Col>
+            <a href="#" className="tertiary-button-xl m-3">
+              Button
+            </a>
+
+            <a href="#" className="tertiary-button-l m-3">
+              Button
+            </a>
+
+            <a href="#" className="tertiary-button-sm m-3">
+              Button
+            </a>
+
+            <a href="#" className="tertiary-button-xsm m-3">
+              Button
+            </a>
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
