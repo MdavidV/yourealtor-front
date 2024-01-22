afda7b05d3bbbecc09b317e84ac85be53a4b32e9
James Gomez <jajogo502@gmail.com>
2024-01-21 22:26:16 -0500

    feat: ajuste carpetas, variables y creacion del form

--- src/contexts/App.jsx
+++ src/contexts/App.jsx
@@ -1,42 +1,9 @@
-import { useState } from "react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
-import "../scss/_main.scss";
-
-import "../App.css";
-import { Button, Col, Container, Row } from "reactstrap";
+import { Button, Col, Container, Row, Form, FormGroup, Input, ListGroup,  ListGroupItem } from "reactstrap";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="titulo">
-        Find Real Estate <br />
-        That Suits You.
-      </h1>
-      <div className="card">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="btn btn-primary"
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-asd">
-        Click on the Vite and React logos to learn more
-      </p>
       <Container>
         <Row>
           <Col>
@@ -98,6 +65,46 @@ function App() {
             </a>
           </Col>
         </Row>
+
+        <Row className="form">
+          <Col className="form_side">
+            <Form className="form_group">
+              <FormGroup className="mb-3">
+                <Input className="form_group_input" type="text" name="userName" id="userName" placeholder="Nombre Completo:" />
+              </FormGroup>
+              <FormGroup>
+                <Input className="form_group_input" type="tel" name="telefono" id="telefono" placeholder="Teléfono:" />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <Input className="form_group_input" type="email" name="email" id="correoElectronico" placeholder="Correo electrónico" />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <Input className="form_group_input" type="textarea" name="mensaje" id="mensaje" placeholder="Mensaje" />
+              </FormGroup>
+              <Button className="secondary-button-xl mt-3">Enviar</Button>
+            </Form>
+          </Col>
+          <Col className="form_side form_info">
+            <ListGroup className="form_info_list">
+              <ListGroupItem className="form_info_list_item">
+                <h2>Información</h2>
+              </ListGroupItem>
+              <ListGroupItem className="form_info_list_item">
+                <i className="bi bi-envelope"></i><a href="mailto:correo@info.com">correo@info.com</a>
+              </ListGroupItem>
+              <ListGroupItem className="form_info_list_item">
+                <i className="bi bi-telephone"></i><a href="tel:+571234567899">+571234567899</a>
+              </ListGroupItem>
+              <ListGroupItem className="form_info_list_item">
+                <i className="bi bi-buildings"></i><p>Av. 26 #12 - 01</p>
+              </ListGroupItem>
+              <ListGroupItem className="form_info_list_item">
+                <i className="bi bi-clock"></i><p>7:00 AM - 6:00 PM</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+        </Row>
+
       </Container>
     </>
   );
