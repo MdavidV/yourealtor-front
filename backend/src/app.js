9e3acc33f1113839afbded6bb7b095216c36d7e4
MdavidV <vinaarizadavid@gmail.com>
2024-03-22 12:48:31 -0500

    Changin server CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173, https://yourealtor-api.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
